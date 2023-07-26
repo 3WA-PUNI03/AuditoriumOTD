62327ccf29e9a714798496de7a7da4f028aeff6e
Kévin Roussel <9155865+KevinRoussel@users.noreply.github.com>
2023-07-26 16:01:00 +0200

    MusicBox time completion

--- Assets/MusicBox.cs
+++ Assets/MusicBox.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MusicBox : MonoBehaviour
 {
-
+    [SerializeField] int _validationThreshold;
     int _particleCount;
+
+    float _completion;      // 0f  ==>   1f
+
+    private void Update()
+    {
+        // Threshold 
+        if(_particleCount > _validationThreshold)
+        {
+            _completion += Time.deltaTime;
+        }
+
+        // Victoire : si la complétion a dépassé XX secondes
+
+
+
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,17 +31,12 @@ public class MusicBox : MonoBehaviour
 
         _particleCount++;
 
-
-
         // Methode 2
         //if (collision.attachedRigidbody.gameObject.CompareTag("Particle") == true)
         //{
         //    _particleCount++;
         //}
-
-
     }
-
 
     private void OnTriggerExit2D(Collider2D collision)
     {
