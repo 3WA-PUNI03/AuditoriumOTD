be813b6f8a02d7a74a1f8b2ac11e7651a72d795f
Kévin Roussel <9155865+KevinRoussel@users.noreply.github.com>
2023-07-26 17:33:11 +0200

    AudioSource volume follows MusicBox's completion

--- Assets/MusicBox.cs
+++ Assets/MusicBox.cs
@@ -2,27 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicBox : MonoBehaviour
 {
     [SerializeField] int _validationThreshold;
-    int _particleCount;
+    [SerializeField] float _objectiveTime;
+    [SerializeField] AudioSource _audio;
 
-    float _completion;      // 0f  ==>   1f
+
+    [Header("DEBUG")]
+    [Header("Le nombre de particule dans la zone")]
+    [SerializeField] int _particleCount;
+    [Header("Le nombre de secondes où l'objectif a été validé")]
+    [SerializeField] float _currentTime;
+    [Header("Le taux de complétion du niveau entre 0 et 1 (0% => 100%)")]
+    [SerializeField] float _currentPercentage;
+
 
     private void Update()
     {
-        // Threshold 
-        if(_particleCount > _validationThreshold)
+        UpdateCompletion();
+
+        CompletionTest();
+    }
+
+    private void CompletionTest()
+    {
+        // Victoire : si la complétion a dépassé XX secondes
+        if (_currentTime > _objectiveTime)
         {
-            _completion += Time.deltaTime;
+            Debug.Log("WIN");
+            //SceneManager.LoadScene("WinScreen");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void UpdateCompletion()
+    {
+        // Threshold 
+        if (_particleCount >= _validationThreshold)
+        {
+            _currentTime += Time.deltaTime;
+        }
+        else
+        {
+            _currentTime -= Time.deltaTime;
+            if (_currentTime < 0)
+            {
+                _currentTime = 0;
+            }
         }
 
-        // Victoire : si la complétion a dépassé XX secondes
-
-
-
-
+        _currentPercentage = _currentTime / _objectiveTime;
+        _audio.volume = _currentPercentage;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
