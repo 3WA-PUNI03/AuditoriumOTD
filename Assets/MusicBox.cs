a66dda6f550fbb2752b7391ee27a49db30b362e4
Kévin Roussel <9155865+KevinRoussel@users.noreply.github.com>
2023-07-26 15:33:54 +0200

    Particle Count

--- Assets/MusicBox.cs
+++ Assets/MusicBox.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class MusicBox : MonoBehaviour
 {
 
+    int _particleCount;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.attachedRigidbody.gameObject.CompareTag("Particle") == false) return;
+
+        _particleCount++;
+
+
+
+        // Methode 2
+        //if (collision.attachedRigidbody.gameObject.CompareTag("Particle") == true)
+        //{
+        //    _particleCount++;
+        //}
+
+
     }
 
-    
+
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.attachedRigidbody.gameObject.CompareTag("Particle") == false) return;
+        _particleCount--;
     }
 
 }
