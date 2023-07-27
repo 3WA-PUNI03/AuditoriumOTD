9e3c2204ec6c9ddccdf14e3ec4838915194c85c5
Kévin Roussel <9155865+KevinRoussel@users.noreply.github.com>
2023-07-27 12:33:19 +0200

    Update scripts

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -7,30 +7,50 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject _prefab;
     [SerializeField] float _fireRate;
+    int _counter;
 
     private void Start()
     {
+        // Pour demander à Unity quand on veut appeller une fonction dans XX secondes
         //Invoke(nameof(SpawnParticle), _fireRate);
         //Invoke("SpawnParticle", _fireRate);
-        //CancelInvoke("SpawnParticle");
+
+
+        // On demande à Unity d'appeller la fonction de Spawn toutes les _fireRate secondes
         InvokeRepeating("SpawnParticle", 0f, _fireRate);
 
-        //Coroutine
+
+        // Version Coroutine un peu plus avancée
         //StartCoroutine(SpawnRoutine());
     }
 
+
+
     void SpawnParticle()
     {
-        //Vector3 randomDirection = new Vector2( Random.Range(-1f, 1f), Random.Range(-1f, 1f) );
+        // Mecanique pour annuler l'appel réccurent une fois que l'on a dépassé un nombre d'appel
+        //_counter++;
+        //if(_counter >= 10)
+        //{
+        //    CancelInvoke("SpawnParticle");
+        //}
+
+        // Position aléatoire dans un cercle pour faire un decalage avec la position du spawner
         Vector3 randomDirection = Random.insideUnitCircle;
+
+        // On calcule la future position de la prochaine particule
         Vector3 destination = transform.position + randomDirection;
+
+        // On instantie la nouvelle particule 
         Instantiate(_prefab, destination, transform.rotation);
 
-        //Invoke("SpawnParticle", _fireRate);
+
+        // Random direction version "carré"
+        //Vector3 randomDirection = new Vector2( Random.Range(-1f, 1f), Random.Range(-1f, 1f) );
     }
 
 
-    // Version coroutine()
+    // Version coroutine, plus avancée que l'on verra plus tard
     IEnumerator SpawnRoutine()
     {
         while(true)
